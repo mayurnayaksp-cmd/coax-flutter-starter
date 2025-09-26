bee26f5097c905608e7fccd7cf0095b1b7142660
mayurnayaksp-cmd <mayur.nayak.sp@gmail.com>
2025-09-26 15:57:12 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,20 @@
 import 'package:flutter/material.dart';
-import 'login_screen.dart';
+import 'splash_screen.dart';
 
 void main() {
-  runApp(const CoaxApp());
+  runApp(const MyApp());
 }
 
-class CoaxApp extends StatelessWidget {
-  const CoaxApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Coax',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(const Duration(seconds: 2), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const LoginScreen()),
-      );
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Coax",
-          style: TextStyle(
-            fontSize: 32,
-            fontWeight: FontWeight.bold,
-            color: Colors.blue,
-          ),
-        ),
-      ),
+      theme: ThemeData.dark(), // dark theme looks modern
+      home: const SplashScreen(), // 👈 start with splash screen
     );
   }
 }
