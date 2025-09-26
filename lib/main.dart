4a6f6a5d3e23d63622c5c34d7cf2957ba224c427
mayurnayaksp-cmd <mayur.nayak.sp@gmail.com>
2025-09-26 17:21:51 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,87 @@
 import 'package:flutter/material.dart';
-import 'splash_screen.dart';
+
+// Import screens
+import 'login_screen.dart';
+import 'home_feed_screen.dart';
+import 'search_screen.dart';
+import 'notifications_screen.dart';
+import 'profile_screen.dart';
+import 'upload_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(CoaxApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class CoaxApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Coax',
-      theme: ThemeData.dark(), // dark theme looks modern
-      home: const SplashScreen(), // 👈 start with splash screen
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: LoginScreen(), // First screen is login
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    HomeFeedScreen(),
+    SearchScreen(),
+    UploadScreen(),
+    NotificationsScreen(),
+    ProfileScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_box_outlined),
+            label: "Upload",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: "Notifications",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+        ],
+      ),
     );
   }
 }
