0aadb47c3d3aa98ec2f01b86901ab51be350eaa7
mayurnayaksp-cmd <mayur.nayak.sp@gmail.com>
2025-09-26 18:29:29 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,28 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
+import 'screens/signup_screen.dart';
 import 'screens/home_screen.dart';
-import 'screens/upload_screen.dart';
-import 'screens/search_screen.dart';
-import 'screens/notifications_screen.dart';
-import 'screens/profile_screen.dart';
 
 void main() {
-  runApp(const CoaxApp());
+  runApp(CoaxApp());
 }
 
 class CoaxApp extends StatelessWidget {
-  const CoaxApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Coax',
       debugShowCheckedModeBanner: false,
+      title: 'Coax',
       theme: ThemeData(
-        primaryColor: Colors.redAccent,
+        primarySwatch: Colors.red,
         scaffoldBackgroundColor: Colors.white,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          titleTextStyle: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-          ),
-          iconTheme: IconThemeData(color: Colors.black),
-        ),
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          backgroundColor: Colors.white,
-          selectedItemColor: Colors.redAccent,
-          unselectedItemColor: Colors.grey,
-          showUnselectedLabels: true,
-          type: BottomNavigationBarType.fixed,
-        ),
       ),
-      home: const LoginScreen(),
-    );
-  }
-}
-
-class MainNavigation extends StatefulWidget {
-  const MainNavigation({super.key});
-
-  @override
-  State<MainNavigation> createState() => _MainNavigationState();
-}
-
-class _MainNavigationState extends State<MainNavigation> {
-  int _currentIndex = 0;
-
-  final List<Widget> _screens = const [
-    HomeScreen(),
-    SearchScreen(),
-    UploadScreen(),
-    NotificationsScreen(),
-    ProfileScreen(),
-  ];
-
-  void _onTap(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _screens[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: _onTap,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
-          BottomNavigationBarItem(icon: Icon(Icons.add_box_outlined), label: "Upload"),
-          BottomNavigationBarItem(icon: Icon(Icons.notifications), label: "Alerts"),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
-        ],
-      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginScreen(),
+        '/signup': (context) => SignUpScreen(),
+        '/home': (context) => HomeScreen(),
+      },
     );
   }
 }
